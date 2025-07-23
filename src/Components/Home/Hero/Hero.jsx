d3c138d93affcdf525ecbfb2b91edf9c3777b538
MohamedAmir520 <mohmdamer520444@gmail.com>
2025-07-23 17:12:45 +0300

    edit1

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -1,5 +1,24 @@
 import React from "react";
-
+import "./Hero.css";
 export default function Hero() {
-  return <div>Hero</div>;
+  return <div>
+    {/* <!-- Section 1 --> */}
+    <div classNameName="section1">
+      <div className="backgroundFull">
+        <div className="box">
+          <div className="inbox">
+            <p className="smalP">New Arrival</p>
+            <p className="bigP">Discover Our New Collection</p>
+            <p className="smalP">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis.
+            </p>
+            <div className="buttonBuy">
+              <button type="button">BUY NOW</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 }
