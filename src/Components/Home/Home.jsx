edc264a6b8aca94f1cc3df01067d07530e3c48bc
Abdelazez Hatem <abdelazezhtm@gmail.com>
2025-07-23 15:45:42 +0300

    remove

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,29 +3,16 @@ import Header from "../Constant/Navbar/Navbar";
 import "./Home.css";
 export default function Home() {
   return (
-<>
-          <Header />
 
-   <div class="section1">
-          <div class="backgroundFull">
-        <div class="box">
-          <div class="inbox">
-            <p class="smalP">New Arrival</p>
-            <p class="bigP">Discover Our New Collection</p>
-            <p class="smalP">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-              tellus, luctus nec ullamcorper mattis.
-            </p>
-            <div class="buttonBuy">
-              <button type="button">BUY NOW</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  
+<dir>
+
+        <Header />
+<h1>home</h1>
+
+</dir>
 
 
-</>
   
   );
 }
