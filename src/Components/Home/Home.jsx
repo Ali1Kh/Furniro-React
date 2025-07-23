3ca709f98808c71f053f7e34042f0a1285639db9
Abdelazez Hatem <abdelazezhtm@gmail.com>
2025-07-23 15:43:41 +0300

    home section one

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 import Header from "../Constant/Navbar/Navbar";
-
+import "./Home.css";
 export default function Home() {
   return (
-    <div>
-      <Header />
-      Home
+<>
+          <Header />
+
+   <div class="section1">
+          <div class="backgroundFull">
+        <div class="box">
+          <div class="inbox">
+            <p class="smalP">New Arrival</p>
+            <p class="bigP">Discover Our New Collection</p>
+            <p class="smalP">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+              tellus, luctus nec ullamcorper mattis.
+            </p>
+            <div class="buttonBuy">
+              <button type="button">BUY NOW</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+
+</>
+  
   );
 }
